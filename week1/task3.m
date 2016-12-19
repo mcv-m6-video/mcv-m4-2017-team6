a1acf3b3c6b6111cb34e47ad7655c1c4029ef553
Arnau <arbamas@gmail.com>
2016-12-19 19:59:55 +0100

    cleaning code

--- week1/task3.m
+++ week1/task3.m
@@ -1,32 +1,5 @@
 function [ ] = task3()
-    close all;
-
-    
-%     GT_PATH = '../training/image_0/';
-%     TEST_PATH = '../testing/image_0/';
-% 
-%     I45_1=im2double(imread(strcat(GT_PATH,'000045_10.png')));
-%     I45_2=im2double(imread(strcat(GT_PATH,'000045_11.png')));
-%     opticalFlow45 = vision.OpticalFlow('Method','Lucas-Kanade','ReferenceFrameSource', 'Input port')                                                          
-%     movement45=step(opticalFlow45, I45_1, I45_2);
-%     
-%    
-%     I157_1=im2double(imread(strcat(GT_PATH,'000157_10.png')));
-%     I157_2=im2double(imread(strcat(GT_PATH,'000157_11.png')));
-%     opticalFlow157 = vision.OpticalFlow('Method','Lucas-Kanade','ReferenceFrameSource', 'Input port')                                                          
-%     movement157= step(opticalFlow157, I157_1, I157_2);
-%
-%     imshow(movement157)
-%     hold on;
-%     [gx,gy] = gradient(movement157);
-%     [Nx, Ny] = size(movement157);
-%     xidx = 1:10:Nx;
-%     yidx = 1:10:Ny;
-%     [X,Y] = meshgrid(xidx,yidx);
-%     quiver(Y',X',abs(gx(xidx,yidx)),abs(gy(xidx,yidx)));
-
-
-
+     close all;
      train45 = imread('../training/flow_noc/000045_10.png');
      [t45u, t45v] = getFlowData(train45);
      res45 = imread('../results/LKflow_000045_10.png');
@@ -54,11 +27,11 @@ function [ ] = task3()
      imshow(sqrt((t157u-r157u + t157v-r157v).^2)>3, [0, 1]);
 end
 
-function [u, v] =getFlowData(flow_img)
-    u = (double(flow_img(:,:,1))-2^15)/64.0;
-    v = (double(flow_img(:,:,2))-2^15)/64.0;
+function [flow_u, flow_v] =getFlowData(flow_img)
+    flow_u = (double(flow_img(:,:,1))-2^15)/64.0;
+    flow_v = (double(flow_img(:,:,2))-2^15)/64.0;
     valid = flow_img(:,:,3)>0;
     
-    u = u.*valid;
-    v = v.*valid;
+    flow_u = flow_u.*valid;
+    flow_v = flow_v.*valid;
 end
