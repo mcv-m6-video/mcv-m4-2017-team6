82d3c98a02369b67a80853fdf4ee84b98bd14061
pau <paucebr@gmail.com>
2016-12-19 19:31:02 +0100

    aa

--- week1/task5.m
+++ week1/task5.m
@@ -1,12 +1,28 @@
 function [ ] = task5()
    
-    I45_1=im2double(imread('../training/imag_0/000045_10.png'));
+    I45_1=im2double(imread('../training/image_0/000045_10.png'));
     train45 = imread('../training/flow_noc/000045_10.png');
     [t45u, t45v] = getFlowData(train45);
 
-    [x, y] = meshgrid(1:size(t45u, 2), 1:size(t45u, 1));
-    quiver(x, y, t45u, t45v, 1.5, 'r', 'LineWidth', 1.2);
+    x = 1:size(t45u, 2);
+    y = 1:size(t45u, 1);
+    
+    downsample = 20;
+    for i=1:size(t45u, 1)
+       for j=1:size(t45u,2)
+           if(mod(j,downsample) ~= 0 || mod(i,downsample)~=0)
+               t45u(i,j) = 0;
+               t45v(i,j) = 0;
+           end
+       end
+    end
+    
+    
+    imshow(I45_1);
+    hold on
+    quiver(x, y, t45u, t45v,100);
     set(gca,'Ydir','reverse')
+    hold off
     
 end
 
