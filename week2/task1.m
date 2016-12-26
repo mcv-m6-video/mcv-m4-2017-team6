d4c77818d5416fb952c6adf20e69c95308416ccd
vcampmany <vcampmany@gmail.com>
2016-12-26 15:09:03 +0100

    task1

--- week2/task1.m
+++ week2/task1.m
@@ -1,22 +1,29 @@
-function [ ] = task1()
+function [ ] = task1(dataset_path)
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
+    dataset_path = strcat(dataset_path, 'dataset/');
+    
     HIGHWAY_SEQ = [1050, 1350];
+    HIGHWAY_PATH = strcat(dataset_path, 'baseline/highway/');
+    
     FALL_SEQ = [1460, 1560];
+    FALL_PATH = strcat(dataset_path, 'dynamicBackground/fall/');
+    
     TRAFFIC_SEQ = [950, 1050];
+    TRAFFIC_PATH = strcat(dataset_path, 'cameraJitter/traffic/');
+    
+    MODEL_SEQ = [1 850];
     MODELING_RATIO = 0.5;
 
-    IN_PATH = '../highway/input/';
     % Model the highway sequence
-    n_samples = int32( (HIGHWAY_SEQ(2) - HIGHWAY_SEQ(1)) * MODELING_RATIO );
+    %n_samples = int32( (HIGHWAY_SEQ(2) - HIGHWAY_SEQ(1)) * MODELING_RATIO );
+    n_samples = int32( MODEL_SEQ(2) - MODEL_SEQ(1) );
     disp('MODELING THE BACKGROUND');
-    [meanP, varP] = task1_bg_modeling(IN_PATH, HIGHWAY_SEQ, n_samples);
+    [meanP, varP] = task1_bg_modeling(HIGHWAY_PATH, MODEL_SEQ, n_samples);
     disp('MODELING DONE');
     
     disp('BACKGROUND ESTIMATION');
-    task1_bg_estimation(IN_PATH, HIGHWAY_SEQ, meanP, varP, n_samples);
-    
-    
+    task1_bg_estimation(HIGHWAY_PATH, HIGHWAY_SEQ, meanP, varP);   
     
 end
 
--- week2/task1_bg_modeling.m
+++ week2/task1_bg_modeling.m
@@ -1,22 +1,24 @@
 function [ p_mean, p_var ] = task1_bg_modeling(IN_PATH, sequence, n_samples)
 %UNTITLED2 Summary of this function goes here
 %   Detailed explanation goes here
-
+    
+    IN_PATH = strcat(IN_PATH, 'input/');
     first = sequence(1);
-    last = first + n_samples;
+    %last = first + n_samples;
+    last = sequence(2);
     
     in = rgb2gray( imread(strcat(IN_PATH, 'in000001.jpg')) );
     seq = zeros(size(in, 1), size(in, 2), n_samples);
     
     for i = first : last
-        in = rgb2gray( imread(strcat(IN_PATH, 'in00', int2str(i), '.jpg')) );
+        in = rgb2gray( imread(strcat(IN_PATH, 'in00', sprintf('%04d',i), '.jpg')) );
         seq(:,:,i) = in(:,:);
-        imshow(in);
-        pause(0.01)
+ %       imshow(in);
+  %      pause(0.01)
     end
     
     p_mean = mean(seq, 3);
-    p_var = std(seq, 0, 3);
+    p_var = var(seq, 0, 3);
     
 
 end
