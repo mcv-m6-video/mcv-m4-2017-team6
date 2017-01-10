195d32e6751581e441c32c5864be6072f1baf2d9
Guillem Cucurull <gcucurull@gmail.com>
2017-01-10 11:41:27 +0100

    Compare task2

--- week2/compare_task2.m
+++ week2/compare_task2.m
@@ -2,8 +2,8 @@ function [ output_args ] = compare_task2( input_args )
 %COMPARE_TASK2 Summary of this function goes here
 %   Detailed explanation goes here
 
-% HIGHWAY (non, ad); FALL (non, ad) ; TRAFFIC (non, ad)
-y = [0.45 0.73; 0.67 0.7 ; 0.48 0.67];
+% HIGHWAY (non, ad, fix-alpha); FALL (non, ad, fix-alpha) ; TRAFFIC (non, ad, fix-alpha)
+y = [0.45 0.73 0.67; 0.67 0.7 0.67; 0.48 0.67 0.53];
 handler = bar(y);
 handler(1).FaceColor = 'b'
 handler(2).FaceColor = 'g'
@@ -14,7 +14,7 @@ set(gca, 'XTickLabel',str, 'XTick',1:numel(str))
 title('Adaptive vs Non-Adaptive');
 xlabel('Highway');
 ylabel('F1 Score');
-legend('Non-Adaptive', 'Adaptive');
+legend('Non-Adaptive', 'Adaptive (Grid Search)', 'Adaptive (Fix Alpha)');
 
 end
 
