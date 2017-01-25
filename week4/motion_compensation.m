2e58cfd4a1eabe55101a910abd85971c2762f800
vcampmany <vcampmany@gmail.com>
2017-01-26 00:33:40 +0100

    task21

--- week4/motion_compensation.m
+++ week4/motion_compensation.m
@@ -1,9 +1,31 @@
-function [ output_args ] = motion_compensation(u, v )
+function [udir_accum, vdir_accum] = motion_compensation(u, v, udir_accum, vdir_accum)
 %UNTITLED4 Summary of this function goes here
 %   Detailed explanation goes here
 
+    u_flat = u(:);
+    v_flat = v(:);
 
-
+    nbins_x = abs(min(u_flat) - max(u_flat));
+    nbins_y = abs(min(v_flat) - max(u_flat));
+    data = [u_flat, v_flat];
+    hist_mat = hist3(data, [nbins_x nbins_y]);
+%     figure
+%     surf(n);
+    %histogram2(u_flat, v_flat);
+    %xlabel('x')
+    %ylabel('y')
+    u_mode = mode(u_flat);
+    v_mode = mode(v_flat);
+    [m n] = max(hist_mat(:));
+    [x y] = ind2sub(size(hist_mat),n);
+    
+    u_dir = x - abs(min(u_flat)); % int32(nbins_x / 2);
+    v_dir = y - abs(min(v_flat)); %int32(nbins_y / 2);
+    u_dir
+    v_dir
+    
+    udir_accum = udir_accum + u_dir;
+    vdir_accum = vdir_accum + v_dir;
 
 end
 
--- week4/task2_1.m
+++ week4/task2_1.m
@@ -8,11 +8,38 @@ function [ ] = task2_1( )
     area_of_search = 16; % P pixels in every direction: (2P+N)x(2P+N) pixels. Typically P = N
     compensation = 'bwd'; % options 'fwd' or 'bwd'
     
+    udir_accum = 0;
+    vdir_accum = 0;
+    first = rgb2gray( imread(strcat(seq_path, 'in00', sprintf('%04d',1), '.jpg')) );
+    %imwrite(first,)
     for i = seq_traffic(1) : seq_traffic(2)-1
+        i
         past = rgb2gray( imread(strcat(seq_path, 'in00', sprintf('%04d',i), '.jpg')) );
         curr = rgb2gray( imread(strcat(seq_path, 'in00', sprintf('%04d',i+1), '.jpg')) );
         flow_result = optical_flow(past, curr, block_size, area_of_search, compensation, 0);   
-        i
+        
+        [udir_accum,vdir_accum] = motion_compensation(flow_result(:,:,1), flow_result(:,:,1), ...
+                                                      udir_accum, vdir_accum);
+        
+        crop = 40;
+        if udir_accum >= 0
+            xstart = crop - udir_accum;
+            xend = size(curr, 2) - crop + udir_accum;
+        else
+            xstart = crop - udir_accum;
+            xend = size(curr, 2) - crop + udir_accum;
+        end
+        
+        if vdir_accum >= 0
+            ystart = crop - vdir_accum;
+            yend = size(curr, 1) - crop + vdir_accum;
+        else
+            ystart = crop - vdir_accum;
+            yend = size(curr, 1) - crop + vdir_accum;
+        end
+        corrected = curr(ystart:yend, xstart:xend);
+        imwrite(corrected, strcat('traffic_corrected/', 'in00', sprintf('%04d',i), '.jpg'));
+        
     end
 
 
