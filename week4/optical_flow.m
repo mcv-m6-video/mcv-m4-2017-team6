b890d201fca421c0516184db4fcd47227c81d023
Guillem Cucurull <gcucurull@gmail.com>
2017-01-25 10:10:31 +0100

    Fix? block matching optical flow and add more comments

--- week4/optical_flow.m
+++ week4/optical_flow.m
@@ -2,51 +2,62 @@ function [ output_args ] = optical_flow( past, current, debug )
 %OPTICAL_FLOW Summary of this function goes here
 %   Detailed explanation goes here
 
-    block_size = 128; % Typically 16x16 pixels
-    area_of_search = 1; % P pixels in every direction: (2P+N)x(2P+N) pixels. Typically P = N
-    compensation = 'fwd';
+    block_size = 30; % Typically 16x16 pixels
+    area_of_search = 30; % P pixels in every direction: (2P+N)x(2P+N) pixels. Typically P = N
+    compensation = 'fwd'; % options 'fwd' or 'bwd'
     % Forward: All pixels in the past image are associated to a  pixel in the current image
     % Backward: All pixels in the current image are associated to a pixel in the past image    
     
+    % Dimensions of the images
     n_rows = size(past,1);
     n_cols = size(past,2);
     
-    % Get the number of blocks per column and row
-    
-    n_blocks_row = floor(n_rows/block_size);
-    n_blocks_col = floor(n_cols/block_size);
-    
+    % Empty structure to save the flow vectors
     flow = zeros(n_rows, n_cols, 2);
     
-    %Idea: in the bucle use im1, or im2 as variable, and define im1 and im2
-    % depending on whether it is forward or backward compensation
+    % Define source and target image depending on the compensation
+    % for forward: we search the blocks in the past image (source) in the
+    % current frame (target)
+    % for backward: we search the blocks in the current image (source) in
+    % the past frame (target)
+    if compensation == 'fwd'
+        source = past;
+        target = current;
+    elseif compensation == 'bwd'
+        source = current;
+        target = past;
+    end
     
+    % Iterate for every block in the source image
     for row_block=0:block_size:n_rows-block_size
-        for col_block = 0:block_size:n_cols-block_size          
-            past_block = past(row_block+1:row_block+block_size, col_block+1:col_block+block_size);
+        for col_block = 0:block_size:n_cols-block_size
+            % Get the block of the source frame we want to find in the
+            % target
+            source_block = source(row_block+1:row_block+block_size, col_block+1:col_block+block_size);
             
-            % debug
+            % To visualize the block matching algorithm
             if debug
-                imshow(past);
+                imshow(source);
                 hold on
                 rectangle('Position',[col_block, row_block, block_size, block_size],'edgeColor','red');
                 hold off;
                 waitforbuttonpress;
             end
-            % end debug
             
-            [flow_row, flow_col] = block_flow(past_block, current, area_of_search, row_block, col_block, debug);
+            % Run the block mathcing algorithm to find the source block in
+            % the target image
+            [flow_row, flow_col] = block_matching(source_block, target, area_of_search, row_block, col_block, debug);
             
-            [flow_row, flow_col]
-            
+            % The block matching algorithm returns a single vector for the
+            % block. Copy this vector to all the pixels belonging to the
+            % block
             flow(row_block+1:row_block+block_size, col_block+1:col_block+block_size,1) = repmat(flow_row, block_size);
             flow(row_block+1:row_block+block_size, col_block+1:col_block+block_size,2) = repmat(flow_col, block_size);
         end
     end
     
-    flow1 = flow(:,:,1);
-    
-    downsample = 20;
+    % Visualize optical flow with quiver
+    downsample = 10; % Show 1 optical flow vectors every 'downsample' pixels
     for i=1:size(flow, 1)
        for j=1:size(flow,2)
            if(mod(j,downsample) ~= 0 || mod(i,downsample)~=0)
@@ -55,24 +66,32 @@ function [ output_args ] = optical_flow( past, current, debug )
            end
        end
     end
-    
-    imshow(current);
+    imshow(target);
     hold on
-    quiver(1:size(flow,2), 1:size(flow,1), flow(:,:,1), flow(:,:,2),100);
+    quiver(1:size(flow,2), 1:size(flow,1), flow(:,:,1), flow(:,:,2),20);
     set(gca,'Ydir','reverse')
     hold off
 
 end
 
-function [flow_row, flow_col] = block_flow(block, image, area_of_search, row_position, col_position, debug)
+function [flow_row, flow_col] = block_matching(block, image, area_of_search, row_position, col_position, debug)
+    % This function is the block matching algorithm
+    % It recieves:
+    %   - block: the block to match
+    %   - image: image to find the block in
+    %   - area_of_search: number of pixels in each side
+    %   - row_position: current position of the block
+    %   - col_position: current position of the block
+    
     block_size = size(block, 1);
     
+    % size of the image
     n_rows = size(image,1);
     n_cols = size(image,2);
     
-    % Compare the given block with all the blocks in the image
-    % restricting by area of search
+    % We have tp Compare the given block restricting by area of search
     
+    % Get the center position of the block
     row_center = floor(row_position+block_size/2);
     col_center = floor(col_position+block_size/2);
     
@@ -80,12 +99,15 @@ function [flow_row, flow_col] = block_flow(block, image, area_of_search, row_pos
     % comparison
     each_side = floor(area_of_search/block_size);
     
-    lower_bound_rows = max(0, row_center-each_side*block_size);
+    % Define the lower and upper bound of the search area in both
+    % dimensions
+    lower_bound_rows = max(0, row_position-each_side*block_size);
     lower_bound_cols = max(0, col_position-each_side*block_size);
     
-    upper_bound_rows = min(n_rows-block_size, row_center+each_side*block_size);
-    upper_bound_cols = min(n_cols-block_size, col_center+each_side*block_size);
+    upper_bound_rows = min(n_rows-block_size, row_position+each_side*block_size);
+    upper_bound_cols = min(n_cols-block_size, col_position+each_side*block_size);
     
+    % To visualize the block matching algorithm
     if debug
         imshow(image);
         hold on
@@ -99,10 +121,13 @@ function [flow_row, flow_col] = block_flow(block, image, area_of_search, row_pos
     flow_row = 0;
     flow_col = 0;
     
+    % Step of the sliding window that search for the matching block
     step_window = 5;
     
+    % Sliding window loop: compare given block against all possible blocks
     for row_block=lower_bound_rows:step_window:upper_bound_rows
         for col_block = lower_bound_cols:step_window:upper_bound_cols
+            % Get current block to compare
             compare_with = double(image(row_block+1:row_block+block_size, col_block+1:col_block+block_size));
             
             % Compute Euclidean distance
@@ -111,32 +136,28 @@ function [flow_row, flow_col] = block_flow(block, image, area_of_search, row_pos
             diff = sum(diff(:)); % sum
             diff = sqrt(diff); %square root
             
-            % debug
+            % To view the block matching algorithm
             if debug
                 rectangle('Position',[col_block, row_block, block_size, block_size],'edgeColor','green');
             end
-            % end debug
             
+            % If the distance is the best until now, update the matched
+            % block information
             if diff < min_dist
                 min_dist = diff;
-                flow_row = row_position - row_block;
-                flow_col = col_position - col_block;
+                flow_row = row_position - row_block; % row flow vector
+                flow_col = col_position - col_block; % column flow vector
                 best_pos_row = row_block;
                 best_pos_col = col_block;
-                most_similar = compare_with;
             end
             
         end
     end
+    % to visualize the block matching algorithm
     if debug
         rectangle('Position',[best_pos_col, best_pos_row, block_size, block_size],'edgeColor','blue');
         waitforbuttonpress;
     end
-    %debug
-%     imshow(most_similar);
-%     waitforbuttonpress;
-    % end debug
-    flow_row = (flow_row + row_position)*(abs(flow_row)>0);
-    flow_col = (flow_col + col_position)*(abs(flow_col)>0);
+    [flow_row, flow_col]
 end
 
