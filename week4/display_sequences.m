9e8a5aae38e2446f67f0c608fa15ac67f306d5d8
vcampmany <vcampmany@gmail.com>
2017-02-03 20:36:11 +0100

    week4 commit

--- week4/display_sequences.m
+++ week4/display_sequences.m
@@ -3,7 +3,7 @@ function [ ] = display_sequences( )
 %   Detailed explanation goes here
     ori_path = 'traffic_dataset/input/original/';
     corr_path = 'traffic_dataset/input/';
-    seq_traffic = [950, 1050];
+    seq_traffic = [1001, 1050];
     
     outputVideo = VideoWriter('traffic_dataset/1_motion_compensation.avi');
     outputVideo.FrameRate = 20;
@@ -12,9 +12,13 @@ function [ ] = display_sequences( )
     for i = seq_traffic(1) : seq_traffic(2)-1
         ori = imread(strcat(ori_path, 'in00', sprintf('%04d',i), '.jpg'));
         corr = imread(strcat(corr_path, 'in00', sprintf('%04d',i), '.jpg'));
+        ori_seg = imread(strcat('foreground_results/original/', 'in00', sprintf('%04d',i), '.png'));
+        corr_seg = imread(strcat('foreground_results/compensated/', 'in00', sprintf('%04d',i), '.png'));
         ori = ori(20:end-20, 20:end-20);
-        corr = corr(20:end-20, 20:end-20);
-        double_i = [ori,corr];
+        ori_seg = ori_seg(20:end-20, 20:end-20);
+        ori_seg = ori_seg * 255;
+        corr_seg = corr_seg * 255;
+        double_i = [ori, corr; ori_seg, corr_seg];
         imshow(double_i);
         
         F = getframe;
--- week4/task2_1.m
+++ week4/task2_1.m
@@ -2,7 +2,8 @@ function [ ] = task2_1( generate_gt)
 %UNTITLED3 Summary of this function goes here
 %   Detailed explanation goes here
     seq_path = '../dataset2014/dataset/cameraJitter/traffic/input/';
-    seq_traffic = [950, 1050];
+    %seq_traffic = [950, 1050];
+    seq_traffic = [1, 1570];
     
     block_size = 16; % Typically 16x16 pixels
     area_of_search = 16; % P pixels in every direction: (2P+N)x(2P+N) pixels. Typically P = N
@@ -11,18 +12,26 @@ function [ ] = task2_1( generate_gt)
     udir_accum = 0;
     vdir_accum = 0;
     cropsize = 20;
-    first = rgb2gray( imread(strcat(seq_path, 'in00', sprintf('%04d',950), '.jpg')) );
-    imwrite(first(cropsize:end-cropsize,cropsize:end-cropsize),strcat('traffic_dataset/input/', 'in00', sprintf('%04d',950), '.jpg'));
+    firstC = imread(strcat(seq_path, 'in000001.jpg'));
+    first = rgb2gray(firstC);
+    imwrite(firstC(cropsize:end-cropsize,cropsize:end-cropsize, :),strcat('traffic_dataset/input/', 'in000001.jpg'));
+    if generate_gt
+        gt = imread('../dataset2014/dataset/cameraJitter/traffic/groundtruth/gt000001.png');
+        imwrite(gt(cropsize:end-cropsize,cropsize:end-cropsize), 'traffic_dataset/groundtruth/gt000001.png');
+    end
+    
     for i = seq_traffic(1) : seq_traffic(2)-1
         i
-        past = rgb2gray( imread(strcat(seq_path, 'in00', sprintf('%04d',i), '.jpg')) );
-        curr = rgb2gray( imread(strcat(seq_path, 'in00', sprintf('%04d',i+1), '.jpg')) );
+        pastC = imread(strcat(seq_path, 'in00', sprintf('%04d',i), '.jpg')); 
+        past = rgb2gray(pastC);
+        currC = imread(strcat(seq_path, 'in00', sprintf('%04d',i+1), '.jpg'));
+        curr = rgb2gray(currC);
         flow_result = optical_flow(past, curr, block_size, area_of_search, compensation, 0);
         
         [udir_accum,vdir_accum]=motion_compensation(flow_result(:,:,2), flow_result(:,:,1), udir_accum, vdir_accum);
         
-        corrected = imtranslate(curr, [udir_accum, vdir_accum]);
-        imwrite(corrected(cropsize:end-cropsize,cropsize:end-cropsize), strcat('traffic_dataset/input/', 'in00', sprintf('%04d',i+1), '.jpg'));
+        corrected = imtranslate(currC, [udir_accum, vdir_accum]);
+        imwrite(corrected(cropsize:end-cropsize,cropsize:end-cropsize, :), strcat('traffic_dataset/input/', 'in00', sprintf('%04d',i+1), '.jpg'));
         
         if generate_gt
             gt = imread(strcat('../dataset2014/dataset/cameraJitter/traffic/groundtruth/', 'gt00', sprintf('%04d',i+1), '.png'));
